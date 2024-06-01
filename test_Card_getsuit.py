bafd45ef5138ecf838c84c4f8e9de70079fe697f
max2311231 <165031286+max2311231@users.noreply.github.com>
2024-06-01 15:42:36 +0200

    Add files via upload
    Sprawdzenie poprawności wpisania nazwy

---
+++ test_Card_getsuit.py
@@ -0,0 +1,11 @@
+import class_Deck
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((20, 80))
+
+d = class_Deck.Deck(screen)
+d.Shuffle()
+
+for _ in range(24):
+    print(d.getCard().getSuit())
